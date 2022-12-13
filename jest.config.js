7daaaf32a92ced2843476f132e3b08b211d3d48f
Willian Vassilek <willz3@outlook.com>
2022-12-13 20:29:25 -0300

    chore: lint fix

--- jest.config.js
+++ jest.config.js
@@ -4,18 +4,18 @@
  */
 
 module.exports = {
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "babel",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   moduleNameMapper: {
-		'@/tests/(.+)': '<rootDir>/tests/$1',
-		'@/(.+)': '<rootDir>/src/$1'
-	},
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
   roots: [
-		"<rootDir>/src",
-    "<rootDir>/tests"
+    '<rootDir>/src',
+    '<rootDir>/tests'
   ],
   transform: {
-		'\\.ts$': 'ts-jest'
-	},
-};
+    '\\.ts$': 'ts-jest'
+  }
+}
